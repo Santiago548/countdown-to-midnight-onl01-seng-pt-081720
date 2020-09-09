ce4cfaff797cc87e439979b275d81b1377ee790a
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-09 21:58:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,12 +6,3 @@ def countdown(num)
     end
     "HAPPY NEW YEAR!"
 end
-
-def countdown_with_sleep(num)
-  num = 10
-    while num >= 10
-      puts "#{num} SECOND(S)!"
-      num -= 1
-    end
-    "HAPPY NEW YEAR!"
-end
